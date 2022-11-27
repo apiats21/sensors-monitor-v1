0e32199c4226e262b1773315a5460fab79f622ab
Andrei Piatsevich <andrei.piatsevich@teamgetter.com>
2022-11-27 09:50:50 +0300

    Initial commit

---
+++ src/main/java/com/supportportal/constant/EmailConstant.java
@@ -0,0 +1,17 @@
+package com.supportportal.constant;
+
+public class EmailConstant {
+    public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtp";
+    public static final String USERNAME = "sensorsmonitorv1@outlook.com";
+    public static final String PASSWORD = "Sun13tech@1";
+    public static final String FROM_EMAIL = "sensorsmonitorv1@outlook.com";
+    public static final String CC_EMAIL = "";
+    public static final String EMAIL_SUBJECT = "Get Arrays, LLC - New Password";
+    public static final String GMAIL_SMTP_SERVER = "smtp-mail.outlook.com";
+    public static final String SMTP_HOST = "mail.smtp.host";
+    public static final String SMTP_AUTH = "mail.smtp.auth";
+    public static final String SMTP_PORT = "mail.smtp.port";
+    public static final int DEFAULT_PORT = 587;
+    public static final String SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
+    public static final String SMTP_STARTTLS_REQUIRED = "mail.smtp.starttls.required";
+}
